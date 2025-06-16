1973b6080df733e91365cb2cc60b1c1607acead8
Brandon Budhan <bran7astic@mac.dhcp.hofstra.edu>
2025-06-16 16:18:12 -0400

    feat: updated all question pairs and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import Flashcard from "./Flashcard";
+import Header from "./Header";
 
 function App() {
   const [index, setIndex] = useState(0);
@@ -8,39 +9,75 @@ function App() {
 
   const nextCard = () => {
     setIndex(Math.floor(Math.random() * 10));
-    setReveal(true);
+    setReveal(false);
   };
 
   const toggleCard = () => {
     setReveal(!reveal);
-    console.log(reveal)
-  }
+    console.log(reveal);
+  };
 
   const cardSet = [
-    { key1: "value1" },
-    { key2: "value2" },
-    { key3: "value3" },
-    { key4: "value4" },
-    { key5: "value5" },
-    { key6: "value6" },
-    { key7: "value7" },
-    { key8: "value8" },
-    { key9: "value9" },
-    { key10: "value10" },
+    {
+      front: "How many total players are there on each team in volleyball?",
+      back: "Twelve players. Two sides, with six players each",
+    },
+    {
+      front: "Which position wears a differently colored jersey?",
+      back: "Libero",
+    },
+    {
+      front:
+        "What is it called when two opposing players simultaneously contact the ball above the net?",
+      back: "A joust",
+    },
+    {
+      front:
+        "How many touches is a team allowed once the ball is on their side?",
+      back: "Three touches",
+    },
+    {
+      front:
+        "What is it called when the ball is spiked over the net, and lands on the floor of the opponent's side? ",
+      back: "A kill",
+    },
+    {
+      front:
+        "What is it called when the score is tied at a point where one team needs two more points to win?",
+      back: "A deuce",
+    },
+    {
+      front: "In beach volleyball, how many players are on either side?",
+      back: "There are two players on each side",
+    },
+    {
+      front:
+        "What is it called when a setter attacks the ball on the second contact, rather than setting it for another player",
+      back: "A dump",
+    },
+    {
+      front: "Which player (ideally) makes the second contact with the ball?",
+      back: "The setter",
+    },
+    { front: 
+        "What is the continuous sequence of plays from the moment the ball is served until a point is scored or a fault is committed called?", 
+      back: "A rally" },
   ];
 
-  const key = Object.keys(cardSet[index])[0];
-  const val = cardSet[index][key];
-  console.log(key, val);
+  // console.log(cardSet[0].key, cardSet[0].val)
+
+  const front = cardSet[index].front;
+  const back = cardSet[index].back;
+  //console.log(key, val);
 
   return (
     <>
-      <h1>Volleyball Flashcards!</h1>
-      <h3 id="">Test your volleyball knowledge with these interactive flashcards!</h3>
-      <h4 id="desc">Number of cards: {cardSet.length}</h4>
-      <br/>
-      <Flashcard text={reveal? key : val} onClick={toggleCard}/>
-      <button className="nextButton" onClick={nextCard}> →</button>
+      <Header amount={cardSet.length} />
+      <Flashcard text={reveal ? back : front} onClick={toggleCard} />
+      <button className="nextButton" onClick={nextCard}>
+        {" "}
+        →
+      </button>
     </>
   );
 }
---
+++ src/Header.jsx
@@ -0,0 +1,24 @@
+import "./App.css"
+
+function Header(props) {
+  return (
+    <>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <img
+          src="volleyball.svg"
+          alt="volleyball icon"
+          id="spinningBall"
+          style={{ height: "50px", width: "50px" }}
+        />
+        <h1>Volleyball Flashcards!</h1>
+      </div>
+      <h3 id="">
+        Test your volleyball knowledge with these interactive flashcards!
+      </h3>
+      <h4 id="desc">Number of cards: {props.amount}</h4>
+      <br />
+    </>
+  );
+}
+
+export default Header;
